f9a8cb17356b42b26ea7145f99c7374d4fbb088d
chu1111 <113088657+chu1111@users.noreply.github.com>
2022-09-10 10:16:21 +0800

    Add files via upload

---
+++ class5.java
@@ -0,0 +1,39 @@
+package java08;
+
+import static java.lang.System.out;
+
+public class class5 {
+
+	public static void main(String[] args) {
+		Cbox box = new Cbox();
+		box.length = 1;
+		box.width = 1;
+		box.height = 1;
+	}
+
+}
+
+class Cbox {
+	int length;
+	int width;
+	int height;
+
+	int volume() {
+		return length * width * height;
+	}
+
+	int SurfaceArea() {
+		return (length * width + length * height + width * height) * 2;
+	}
+
+	void showData() {
+		out.printf("%d %d %d", length, width, height);
+	}
+
+	void showAll() {
+		showData();
+		out.print(SurfaceArea());
+		out.print(volume());
+
+	}
+}
